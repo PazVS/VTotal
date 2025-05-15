8a0071ec8b4fe26a2bc1cc109b182f9fa9a4c3ea
PazVS <pachugiacchino@gmail.com>
2025-05-15 12:23:44 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const path = require('path');
+const app = express();
+
+const VIRUSTOTAL_API_KEY = '85cf97a6b2db72efc28aa90d39a79e24740bbcc231e99ce46ed41f79b0140490';
+
+app.use(express.json());
+
+// Sirve archivos estáticos desde la carpeta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta para procesar las solicitudes a VirusTotal
+app.post('/api/check-hash', async (req, res) => {
+  const { hash } = req.body;
+
+  try {
+    const response = await fetch(`https://www.virustotal.com/api/v3/files/${hash}`, {
+      headers: {
+        'x-apikey': VIRUSTOTAL_API_KEY
+      }
+    });
+
+    if (!response.ok) {
+      return res.status(response.status).json({ error: `Error en la solicitud a VirusTotal: ${response.status}` });
+    }
+
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: 'Error procesando el hash' });
+  }
+});
+
+
+app.post('/api/convert-hash', async (req, res) => {
+  const { hash } = req.body;
+
+  try {
+    const response = await fetch(`https://www.virustotal.com/api/v3/files/${hash}`, {
+      headers: {
+        'x-apikey': VIRUSTOTAL_API_KEY
+      }
+    });
+
+    if (!response.ok) {
+      return res.status(response.status).json({ error: `Error en la solicitud a VirusTotal: ${response.status}` });
+    }
+
+    const data = await response.json();
+    const sha256 = data.data.id; // Extraer el SHA-256 de la respuesta
+    res.json({ sha256 });
+  } catch (error) {
+    res.status(500).json({ error: 'Error procesando la conversión del hash' });
+  }
+});
+
+
+// Ruta para servir el archivo index.html por defecto
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
